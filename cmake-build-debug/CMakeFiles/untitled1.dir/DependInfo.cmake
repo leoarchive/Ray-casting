862e42653fd453ef0058c6c1b72c5f70d03781e5
Leonardo Nunes <67336436+leozamboni@users.noreply.github.com>
2020-11-17 00:40:07 -0300

    c

--- cmake-build-debug/CMakeFiles/untitled1.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/untitled1.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "L:/Usuarios/ProjetosC/untitled1/main.c" "L:/Usuarios/ProjetosC/untitled1/cmake-build-debug/CMakeFiles/untitled1.dir/main.c.obj"
+  "L:/Usuarios/Github Storage/Ray-casting/main.c" "L:/Usuarios/Github Storage/Ray-casting/cmake-build-debug/CMakeFiles/untitled1.dir/main.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: L:/Usuarios/ProjetosC/untitled1
+# Install script for directory: L:/Usuarios/Github Storage/Ray-casting
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "L:/Usuarios/ProjetosC/untitled1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "L:/Usuarios/Github Storage/Ray-casting/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
